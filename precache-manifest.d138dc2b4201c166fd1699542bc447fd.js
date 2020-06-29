6d9dc6fed89492dd2eb2e4fdebdffa0ba817c9c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-29 17:54:15 +0000

    Deploy todack/todack.github.io to github.com/todack/todack.github.io.git:master

---
+++ precache-manifest.d138dc2b4201c166fd1699542bc447fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9197772665fcdbb81835",
+    "url": "/css/app.6e443122.css"
+  },
+  {
+    "revision": "5a771255544c0e155d8e",
+    "url": "/css/chunk-vendors.96c3af86.css"
+  },
+  {
+    "revision": "fa2eb5d7f5fa194d7a7ad4700b5715bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9197772665fcdbb81835",
+    "url": "/js/app.af9b639d.js"
+  },
+  {
+    "revision": "5a771255544c0e155d8e",
+    "url": "/js/chunk-vendors.9294cdd6.js"
+  },
+  {
+    "revision": "066eadb51956483bf60a1220636ba071",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
